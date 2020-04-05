632046eb79861c22ce321dc21493c0f53c402cd9
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-04-05 15:41:56 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/teste', (req, res) => {
-    return res.json({hello: 'word'})
+    return res.json({hello: 'word 2'})
 });
 
 app.listen(3333);
